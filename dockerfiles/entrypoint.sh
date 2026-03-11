48237680056355cba41beeadf78d9eab923f900c
Khang Pham <phamthekhang632@gmail.com>
2026-03-11 15:58:57 +0900

    chore: update submodules & delete trailing white spaces

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -16,21 +16,21 @@ echo "--- Compiling mc_robots ---"
 mkdir -p /root/mc_robots/build
 cd /root/mc_robots/build
 cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
-make install 
+make install
 echo "--- Complete Compiling mc_robots ---"
 
 echo "--- Compiling controllers ---"
 mkdir -p /root/mc_rtc_ws/build
 cd /root/mc_rtc_ws/build
 cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
-make install 
+make install
 echo "--- Complete Compiling robots_description ---"
 
 echo "--- Compiling mc_interface ---"
 mkdir -p /root/mc_interface/build
 cd /root/mc_interface/build
 cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
-make install 
+make install
 echo "--- Complete Compiling mc_interface ---"
 
 echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
@@ -38,4 +38,4 @@ echo "source /root/colcon_ws/install/setup.bash" >> ~/.bashrc
 
 echo "--- mc_rtc_tutorials is ready ---"
 
-exec "$@" 
+exec "$@"
